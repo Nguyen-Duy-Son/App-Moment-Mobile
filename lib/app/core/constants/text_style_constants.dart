498f6afb71a2f9480f80179c4782c0a99e5947f4
Thi <dinhthi1440@gmail.com>
2024-06-27 11:03:56 +0700

    add font and color const

--- lib/app/core/constants/text_style_constants.dart
+++ lib/app/core/constants/text_style_constants.dart
@@ -5,4 +5,96 @@ import 'package:google_fonts/google_fonts.dart';
 class TextStyleConstant {
   static TextStyle boldLight32 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 32.w, color: Colors.black);
   static TextStyle boldDark32 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 32.w, color: Colors.white);
+  static TextStyle regularLight32 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 32.w, color: Colors.black);
+  static TextStyle regularDark32 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 32.w, color: Colors.white);
+  static TextStyle lightLight32 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 32.w, color: Colors.black);
+  static TextStyle lightDark32 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 32.w, color: Colors.white);
+
+  // 30
+  static TextStyle boldLight30 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 30.w, color: Colors.black);
+  static TextStyle boldDark30 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 30.w, color: Colors.white);
+  static TextStyle regularLight30 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 30.w, color: Colors.black);
+  static TextStyle regularDark30 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 30.w, color: Colors.white);
+  static TextStyle lightLight30 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 30.w, color: Colors.black);
+  static TextStyle lightDark30 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 30.w, color: Colors.white);
+
+  // 28
+  static TextStyle boldLight28 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 28.w, color: Colors.black);
+  static TextStyle boldDark28 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 28.w, color: Colors.white);
+  static TextStyle regularLight28 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 28.w, color: Colors.black);
+  static TextStyle regularDark28 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 28.w, color: Colors.white);
+  static TextStyle lightLight28 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 28.w, color: Colors.black);
+  static TextStyle lightDark28 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 28.w, color: Colors.white);
+
+  // 26
+  static TextStyle boldLight26 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 26.w, color: Colors.black);
+  static TextStyle boldDark26 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 26.w, color: Colors.white);
+  static TextStyle regularLight26 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 26.w, color: Colors.black);
+  static TextStyle regularDark26 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 26.w, color: Colors.white);
+  static TextStyle lightLight26 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 26.w, color: Colors.black);
+  static TextStyle lightDark26 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 26.w, color: Colors.white);
+
+  // 24
+  static TextStyle boldLight24 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 24.w, color: Colors.black);
+  static TextStyle boldDark24 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 24.w, color: Colors.white);
+  static TextStyle regularLight24 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 24.w, color: Colors.black);
+  static TextStyle regularDark24 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 24.w, color: Colors.white);
+  static TextStyle lightLight24 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 24.w, color: Colors.black);
+  static TextStyle lightDark24 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 24.w, color: Colors.white);
+
+  // 22
+  static TextStyle boldLight22 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 22.w, color: Colors.black);
+  static TextStyle boldDark22 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 22.w, color: Colors.white);
+  static TextStyle regularLight22 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 22.w, color: Colors.black);
+  static TextStyle regularDark22 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 22.w, color: Colors.white);
+  static TextStyle lightLight22 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 22.w, color: Colors.black);
+  static TextStyle lightDark22 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 22.w, color: Colors.white);
+
+  // 20
+  static TextStyle boldLight20 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 20.w, color: Colors.black);
+  static TextStyle boldDark20 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 20.w, color: Colors.white);
+  static TextStyle regularLight20 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 20.w, color: Colors.black);
+  static TextStyle regularDark20 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 20.w, color: Colors.white);
+  static TextStyle lightLight20 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 20.w, color: Colors.black);
+  static TextStyle lightDark20 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 20.w, color: Colors.white);
+
+  // 18
+  static TextStyle boldLight18 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 18.w, color: Colors.black);
+  static TextStyle boldDark18 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 18.w, color: Colors.white);
+  static TextStyle regularLight18 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 18.w, color: Colors.black);
+  static TextStyle regularDark18 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 18.w, color: Colors.white);
+  static TextStyle lightLight18 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 18.w, color: Colors.black);
+  static TextStyle lightDark18 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 18.w, color: Colors.white);
+
+  // 16
+  static TextStyle boldLight16 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 16.w, color: Colors.black);
+  static TextStyle boldDark16 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 16.w, color: Colors.white);
+  static TextStyle regularLight16 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 16.w, color: Colors.black);
+  static TextStyle regularDark16 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 16.w, color: Colors.white);
+  static TextStyle lightLight16 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 16.w, color: Colors.black);
+  static TextStyle lightDark16 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 16.w, color: Colors.white);
+
+  // 14
+  static TextStyle boldLight14 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 14.w, color: Colors.black);
+  static TextStyle boldDark14 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 14.w, color: Colors.white);
+  static TextStyle regularLight14 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 14.w, color: Colors.black);
+  static TextStyle regularDark14 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 14.w, color: Colors.white);
+  static TextStyle lightLight14 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 14.w, color: Colors.black);
+  static TextStyle lightDark14 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 14.w, color: Colors.white);
+
+  // 12
+  static TextStyle boldLight12 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 12.w, color: Colors.black);
+  static TextStyle boldDark12 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 12.w, color: Colors.white);
+  static TextStyle regularLight12 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 12.w, color: Colors.black);
+  static TextStyle regularDark12 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 12.w, color: Colors.white);
+  static TextStyle lightLight12 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 12.w, color: Colors.black);
+  static TextStyle lightDark12 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 12.w, color: Colors.white);
+
+  // 10
+  static TextStyle boldLight10 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 10.w, color: Colors.black);
+  static TextStyle boldDark10 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w700, fontSize: 10.w, color: Colors.white);
+  static TextStyle regularLight10 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 10.w, color: Colors.black);
+  static TextStyle regularDark10 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w400, fontSize: 10.w, color: Colors.white);
+  static TextStyle lightLight10 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 10.w, color: Colors.black);
+  static TextStyle lightDark10 = GoogleFonts.dongle().copyWith(fontWeight: FontWeight.w300, fontSize: 10.w, color: Colors.white);
 }
